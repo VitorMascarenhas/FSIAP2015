ddc6cade81adb0ad42df43c7839551c98059ec6f
1081320 <1081320@isep.ipp.pt>
2015-10-27 22:09:09 +0000

    Project Initialization - Starting....

---
+++ FISIAP_Project_1516_G03/src/fisiap_project_1516_g03/FISIAP_Project_1516_G03.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fisiap_project_1516_g03;
+
+/**
+ *
+ * @author E336170
+ */
+public class FISIAP_Project_1516_G03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
