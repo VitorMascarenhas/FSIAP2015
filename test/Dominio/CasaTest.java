1a55a80afcd3073a5ac48e7de14c15bb17df9275
Joel Alves <joel.17.alves@gmail.com>
2015-11-19 21:36:31 +0000

    fix adicionar classes de teste

--- test/Dominio/CasaTest.java
+++ test/Dominio/CasaTest.java
@@ -16,12 +16,14 @@ import org.junit.Before;
  * @author vitoralexandremascarenhasmascarenhas
  */
 public class CasaTest {
+    public Casa casa2;
     
     public CasaTest() {
     }
 
     @Before
     public void setUp() throws Exception {
+        casa2=new Casa(20f,10f);
     }
 
     @After
@@ -45,7 +47,7 @@ public class CasaTest {
         float altura = (float) 3;
         float largura = (float) 9;
         
-        Casa casa1=new Casa();
+        Casa casa1=new Casa(20f,10f);
         
         Materiais instance = Materiais.getInstance();
         instance.inserirMaterial("Ferro", 80.2F);
@@ -97,8 +99,9 @@ public class CasaTest {
         casa1.adicionarParede(parede3);
         casa1.adicionarParede(parede4);
         
+        
         System.out.println(casa1.calculaResistenciaTermicaTotal());
-        double resultado = 0.00155;
+        double resultado = 4.1145317E-6;
         assertEquals("CalculaResistenciaTermicaTotal", resultado, casa1.calculaResistenciaTermicaTotal(), 0.00001);
     }
 
@@ -107,6 +110,64 @@ public class CasaTest {
      */
     @Test
     public void testCalculaFluxoCalor() {
+        System.out.println("CalculaFluxoCalor");
+        
+        float altura = (float) 3;
+        float largura = (float) 9;
+        Casa casa1=new Casa(20f,10f);
+        
+        Materiais instance = Materiais.getInstance();
+        instance.inserirMaterial("Ferro", 80.2F);
+        instance.inserirMaterial("Cimento", 8.2F);
+        instance.inserirMaterial("Tijolo", 10.2F);
+        instance.inserirMaterial("Vidro", 15.2F);
+        instance.inserirMaterial("Madeira", 5.2F);
+        
+        Parede parede1 = new Parede();
+        Camada camada11 = new Camada(altura, largura, 0.006f, "Ferro");
+        Camada camada12 = new Camada(altura, largura, 0.0012f, "Cimento");
+        Camada camada13 = new Camada(altura, largura, 0.0012f, "Tijolo");
+        parede1.adicionarCamada(camada11);
+        parede1.adicionarCamada(camada12);
+        parede1.adicionarCamada(camada13);
+        
+        Parede parede2 = new Parede();
+        Camada camada21 = new Camada(altura, largura, 0.006f, "Ferro");
+        Camada camada22 = new Camada(altura, largura, 0.0012f, "Cimento");
+        Camada camada23 = new Camada(altura, largura, 0.0012f, "Tijolo");
+        parede2.adicionarCamada(camada21);
+        parede2.adicionarCamada(camada22);
+        parede2.adicionarCamada(camada23);
+        
+        Parede parede3 = new Parede();
+        Camada camada31 = new Camada(altura, largura, 0.006f, "Ferro");
+        Camada camada32 = new Camada(altura, largura, 0.0012f, "Cimento");
+        Camada camada33 = new Camada(altura, largura, 0.0012f, "Tijolo");
+        parede3.adicionarCamada(camada31);
+        parede3.adicionarCamada(camada32);
+        parede3.adicionarCamada(camada33);
+        
+        Parede parede4 = new Parede();
+        Camada camada41 = new Camada(altura, largura, 0.006f, "Ferro");
+        Camada camada42 = new Camada(altura, largura, 0.0012f, "Cimento");
+        Camada camada43 = new Camada(altura, largura, 0.0012f, "Tijolo");
+        Janela janela41 = new Janela(1f, 1.5f, 0.0012f, "Vidro");
+        Janela janela42 = new Janela(1f, 1.1f, 0.0012f, "Vidro");
+        Porta porta41 =  new Porta(2f, 1f, 0.0012f, "Madeira");
+        parede4.adicionarCamada(camada41);
+        parede4.adicionarCamada(camada42);
+        parede4.adicionarCamada(camada43);
+        parede4.adicionarJanela(janela41);
+        parede4.adicionarJanela(janela42);
+        parede4.adicionarPorta(porta41);
+        
+        casa1.adicionarParede(parede1);
+        casa1.adicionarParede(parede2);
+        casa1.adicionarParede(parede3);
+        casa1.adicionarParede(parede4);
+        System.out.println(casa1.calculaFluxoCalor());
+        double resultado =2430410.25;
+        assertEquals("CalculaFluxoCalor", resultado, casa1.calculaFluxoCalor(), 0);
     }
 
     /**
