8f8ebc2e8916479e6060edd5b16adc91288404cc
1100912 <1100912@isep.ipp.pt>
2015-11-22 16:24:46 +0000

    criacao layouts

--- src/UI/StartExperience.java
+++ src/UI/StartExperience.java
@@ -5,7 +5,10 @@
  */
 package UI;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
 import java.awt.Dimension;
+import javax.swing.*;
 import javax.swing.JInternalFrame;
 
 /**
@@ -13,26 +16,68 @@ import javax.swing.JInternalFrame;
  * @author Nuno Lemos
  */
 public class StartExperience extends JInternalFrame {
-    
-    
+    private JTextField temperaturaExterior, temperaturaInterior;
+    private JLabel label_temperaturaExterior, label_temperaturaInterior, label_html, label_separadores;
 
-    //construtor
-    public StartExperience (){
+    public StartExperience() {
         super("StartExperience",
           true, //resizable
           true, //closable
           true, //maximizable
           true);//iconifiable
         
+        Container contentPane = getContentPane();
+        //Use the content pane's default BorderLayout.
+        contentPane.setLayout(new BorderLayout()); //unnecessary
+
+        JPanel panelLeft = new JPanel();
+        
+        JPanel panelLeftNorth = new JPanel();
+        
+        label_temperaturaExterior = new JLabel("Temperatura Exterior");
+        temperaturaExterior = new JTextField(5);
+        label_temperaturaInterior = new JLabel("Temperatura Interior");
+        temperaturaInterior = new JTextField(5);
+        
+        panelLeftNorth.add(label_temperaturaExterior);
+        panelLeftNorth.add(temperaturaExterior);
+        panelLeftNorth.add(label_temperaturaInterior);
+        panelLeftNorth.add(temperaturaInterior);
         
         
         
+        JPanel panelLeftSouth = new JPanel();
+        
+        label_html = new JLabel("Espaço reservado para HTML");
+        panelLeftSouth.add(label_html);
+        
+        
+        
+        
+        panelLeft.add(panelLeftNorth,
+                        BorderLayout.NORTH);
+        panelLeft.add(panelLeftSouth,
+                        BorderLayout.CENTER);
+        
+        
+        JPanel panelRight = new JPanel();
+        
+        label_separadores = new JLabel("Espaço reservado para Separadores");
+        panelRight.add(label_separadores);
+        
+        
+        
+        contentPane.add(panelLeft,
+                        BorderLayout.CENTER);
+        contentPane.add(panelRight,
+                        BorderLayout.EAST);
+                
         //define o frame
-        setSize(1000, 800);
-        setClosable(true);
-        setResizable(true);
+        setSize(800, 600);
+        setMinimumSize(new Dimension(800, 600));
+        Dimension paneSize = contentPane.getSize();   
+        Dimension screenSize = contentPane.getToolkit().getScreenSize();   
+        
         setVisible(true);
-        setMinimumSize(new Dimension(1000, 800));
     }
-    
 }
