a89a44460fc63e332e66fc72f6cb2eee369dc459
vitoralexandremascarenhasmascarenhas <vitoralexandremascarenhasmascarenhas@mbp-de-vitor>
2015-11-23 19:08:07 +0000

    criação dos separadores

--- src/UI/StartExperience.java
+++ src/UI/StartExperience.java
@@ -6,10 +6,14 @@
 package UI;
 
 import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import javax.swing.*;
 import javax.swing.JInternalFrame;
 
@@ -31,8 +35,18 @@ public class StartExperience extends JInternalFrame {
         Container contentPane = getContentPane();
         //Use the content pane's default BorderLayout.
         contentPane.setLayout(new BorderLayout()); //unnecessary
-
-        JPanel panelLeft = new JPanel();
+        
+        JPanel painelNorte = new JPanel();
+        
+        GridBagConstraints constDimTmp = new GridBagConstraints();
+        GridBagLayout layoutDimTmp = new GridBagLayout();
+        painelNorte.setLayout(layoutDimTmp);
+        
+        contentPane.add(painelNorte, BorderLayout.NORTH);
+        
+        JPanel painelDireito = new JPanel();
+        contentPane.add(painelDireito, BorderLayout.EAST);
+        //JPanel panelLeft = new JPanel();
         
         // paineis para dados das paredes
         JPanel painelParedeA = new JPanel();
@@ -43,22 +57,52 @@ public class StartExperience extends JInternalFrame {
         JPanel painelTeto = new JPanel();
         
         JLabel lblAlturaPrdA = new JLabel("Altura");
-        JLabel lblAlturaValPrdA = new JLabel();
+        JLabel lblAlturaValPrdA = new JLabel("__");
         JLabel lblLarguraPrdA = new JLabel("Largura");
-        JLabel lblLarguraValPrdA = new JLabel();
+        JLabel lblLarguraValPrdA = new JLabel("__");
         JLabel lblEspessuraPrdA = new JLabel("Espessura");
         JTextField txtEspCamPrdA = new JTextField();
         JLabel lblMaterialPrdA = new JLabel("Material");
         JComboBox cmbMaterialPrdA = new JComboBox();
         JButton btnValidaCamadaA = new JButton("Valida Parede");
         
-        painelParedeA.setLayout(new FlowLayout());
-        painelParedeA.add(lblAlturaPrdA);
-        painelParedeA.add(lblAlturaValPrdA);
-        painelParedeA.add(lblLarguraPrdA);
-        painelParedeA.add(lblLarguraValPrdA);
+        GridBagConstraints constParedeA = new GridBagConstraints();
+        GridBagLayout layoutParedeA = new GridBagLayout();
+        painelParedeA.setLayout(layoutParedeA);
+        constParedeA.insets = new Insets(10, 10, 10, 10);
+        painelParedeA.add(lblAlturaPrdA, constParedeA);
+        constParedeA.gridx = 1;
+        painelParedeA.add(lblLarguraPrdA, constParedeA);
+        constParedeA.gridx = 2;
+        painelParedeA.add(lblEspessuraPrdA, constParedeA);
+        constParedeA.gridx = 3;
+        painelParedeA.add(lblMaterialPrdA, constParedeA);
+        constParedeA.gridx = 0;
+        constParedeA.gridy = 1;
         
+        painelParedeA.add(lblAlturaValPrdA, constParedeA);
+        constParedeA.gridx = 1;
+        painelParedeA.add(lblLarguraValPrdA, constParedeA);
+        constParedeA.gridx = 2;
+        painelParedeA.add(txtEspCamPrdA, constParedeA);
+        constParedeA.gridx = 3;
+        painelParedeA.add(cmbMaterialPrdA, constParedeA);
+        // Cria o objeto parede
+        JButton btnValidarParedeA = new JButton("Validar parede A");
+        constParedeA.gridx = 0;
+        constParedeA.gridy = 2;
+        constParedeA.gridheight = 3;
+        painelParedeA.add(btnValidaCamadaA, constParedeA);
+        constParedeA.gridy = 3;
+        constParedeA.gridx = 0;
+        JLabel lblLstCompPrdA = new JLabel("Lista de componentes");
+        constParedeA.gridy = 4;
+        painelParedeA.add(lblLstCompPrdA, constParedeA);
+        JList lstCompPrdA = new JList();
+        constParedeA.gridy = 5;
+        painelParedeA.add(lstCompPrdA, constParedeA);
         
+        constDimTmp.gridx = 6;
         JTabbedPane separadores = new JTabbedPane();
         separadores.add(painelParedeA, 0);
         separadores.setTitleAt(0, "Parede A");
@@ -73,12 +117,20 @@ public class StartExperience extends JInternalFrame {
         separadores.add(painelTeto, 5);
         separadores.setTitleAt(5, "Této");
         separadores.setMaximumSize(null);
+        constDimTmp.gridy = 3;
+        painelDireito.add(separadores, constDimTmp);
         
+        
+        /*
         JPanel painelSeparadores = new JPanel();
         painelSeparadores.add(separadores);
-        
+        */
+        /*
         JPanel panelLeftNorth = new JPanel();
         panelLeftNorth.setLayout( new GridLayout(2,10) );
+        */
+        constDimTmp.gridx = 0;
+        constDimTmp.gridy = 1;
         
         label_temperaturas = new JLabel("Temperaturas:");
         label_temperaturaExterior = new JLabel("Exterior");
@@ -88,6 +140,23 @@ public class StartExperience extends JInternalFrame {
         label_temperaturaSolo = new JLabel("Solo");
         temperaturaSolo = new JTextField(5);
         
+        constDimTmp.gridx = 0;
+        constDimTmp.gridy = 0;
+        
+        painelNorte.add(label_temperaturas, constDimTmp);
+        constDimTmp.gridx = 1;
+        painelNorte.add(label_temperaturaExterior, constDimTmp);
+        constDimTmp.gridx = 2;
+        painelNorte.add(temperaturaExterior, constDimTmp);
+        constDimTmp.gridx = 3;
+        painelNorte.add(label_temperaturaInterior, constDimTmp);
+        constDimTmp.gridx = 4;
+        painelNorte.add(temperaturaInterior, constDimTmp);
+        constDimTmp.gridx = 5;
+        painelNorte.add(label_temperaturaSolo, constDimTmp);
+        constDimTmp.gridx = 6;
+        painelNorte.add(temperaturaSolo, constDimTmp);
+        
         label_dimensoes = new JLabel("Dimensões Casa:");
         label_comprimentoCasa = new JLabel("Comprimento");
         comprimentoCasa = new JTextField(5);
@@ -96,6 +165,30 @@ public class StartExperience extends JInternalFrame {
         label_alturaCasa = new JLabel("Altura");
         alturaCasa = new JTextField(5);
         
+        constDimTmp.gridx = 0;
+        constDimTmp.gridy = 2;
+        
+        painelNorte.add(label_dimensoes, constDimTmp);
+        constDimTmp.gridx = 1;
+        painelNorte.add(label_alturaCasa, constDimTmp);
+        constDimTmp.gridx = 2;
+        painelNorte.add(alturaCasa, constDimTmp);
+        constDimTmp.gridx = 3;
+        painelNorte.add(label_larguraCasa, constDimTmp);
+        constDimTmp.gridx = 4;
+        painelNorte.add(larguraCasa, constDimTmp);
+        constDimTmp.gridx = 5;
+        painelNorte.add(label_comprimentoCasa, constDimTmp);
+        constDimTmp.gridx = 6;
+        painelNorte.add(comprimentoCasa, constDimTmp);
+        constDimTmp.gridx = 7;
+        
+        constDimTmp.gridx = 6;
+        constDimTmp.gridy = 4;
+        painelNorte.add(separadores, constDimTmp);
+        
+        
+                /*
         panelLeftNorth.add(label_temperaturas);
         panelLeftNorth.add(label_temperaturaExterior);
         panelLeftNorth.add(temperaturaExterior);
@@ -110,8 +203,8 @@ public class StartExperience extends JInternalFrame {
         panelLeftNorth.add(larguraCasa);
         panelLeftNorth.add(label_alturaCasa);
         panelLeftNorth.add(alturaCasa);
-        
-        
+        */
+        /*
         JPanel panelLeftSouth = new JPanel();
         
         label_html = new JLabel("Espaço reservado para HTML");
@@ -122,19 +215,19 @@ public class StartExperience extends JInternalFrame {
         
         panelLeft.add(panelLeftNorth, BorderLayout.NORTH);
         panelLeft.add(panelLeftSouth, BorderLayout.CENTER);
+        */
         
-        
-        JPanel panelRight = new JPanel();
+        //JPanel panelRight = new JPanel();
         
         //label_separadores = new JLabel("Espaço reservado para Separadores");
         //panelRight.add(label_separadores);
-        panelRight.setLayout(new BorderLayout());
+        /*panelRight.setLayout(new BorderLayout());
         panelRight.add(painelSeparadores, BorderLayout.NORTH);
         panelRight.add(painelParedeA, BorderLayout.CENTER);
+        */
         
-        
-        contentPane.add(panelLeft, BorderLayout.CENTER);
-        contentPane.add(panelRight, BorderLayout.EAST);
+        //contentPane.add(panelLeft, BorderLayout.CENTER);
+        //contentPane.add(panelRight, BorderLayout.EAST);
                 
         //define o frame
         setSize(800, 600);
