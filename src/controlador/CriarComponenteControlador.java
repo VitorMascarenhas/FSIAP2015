1b84211976b1553689e9d4bb0ec3228ec7f5c78e
vitoralexandremascarenhasmascarenhas <vitoralexandremascarenhasmascarenhas@172.18.134.120>
2015-11-27 15:01:45 +0000

    controlador para criar componentes

---
+++ src/controlador/CriarComponenteControlador.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import Dominio.*;
+import java.util.*;
+import Factorys.*;
+import Repositorios.Materiais;
+
+/**
+ *
+ * @author vitoralexandremascarenhasmascarenhas
+ */
+public class CriarComponenteControlador {
+    
+    public CriarComponenteControlador() {
+    }
+    
+    public Componente criarComponente(String nomeComponente, String altura, String largura, String espessura, String tipoMaterial) {
+        
+        float altCmp = Float.parseFloat(altura);
+        float larCmp = Float.parseFloat(largura);
+        float espCmp = Float.parseFloat(espessura);
+        
+        FabricaComponentes fab = new FabricaComponentes();
+        
+        return fab.getComponente(nomeComponente, altCmp, larCmp, espCmp, tipoMaterial);
+    }
+}
